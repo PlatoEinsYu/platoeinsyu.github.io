12f11e1fd3bd2fb9d114529dfd85476d5d5067bf
PlatoEinsYu <66504779+PlatoEinsYu@users.noreply.github.com>
2025-04-08 05:24:42 +0000

    Deploying to gh-pages from @ PlatoEinsYu/platoeinsyu.github.io@ab2dd1c291f8c14efe8632a2ac4d7f42026c9bb4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,18 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-a-post-with-math",
-      
-        title: "a post with math",
-      
-      description: "an example of a blog post with some math",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2015/math/";
-        
-      },
-    },{id: "news-a-simple-inline-announcement",
+        },{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
           section: "News",},{id: "projects-project-2",
